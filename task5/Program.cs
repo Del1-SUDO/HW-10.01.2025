9d283aafa40864c5bc17ab4ba205618ac45d64af
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-10 14:17:45 +0500

    done

---
+++ task5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program {
+    static void Main() {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(input);
+        if (isPalindrome) { 
+            Console.WriteLine("Палиндром: Да"); 
+        } 
+        else { 
+            Console.WriteLine("Палиндром: Нет"); 
+        }
+    }
+
+    static bool IsPalindrome(string str) {
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right) {
+            if (str[left] != str[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
