5cae3247e0e4eb5ce3ceea97e6816de922635979
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-10 13:27:00 +0500

    task 9 is done

---
+++ task9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Enter the number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            numbers[i] = rand.Next(1, 51);
+            Console.Write(numbers[i] + " ");
+        }
+        Console.WriteLine();
+
+        double average = total(numbers);
+        Console.WriteLine("Среднее значение: " + average);
+    }
+
+    static double total(int[] numbers) {
+        double sum = 0;
+        foreach (int num in numbers) {
+            sum+=num;
+        }
+        return sum/numbers.Length;
+    }
+}
