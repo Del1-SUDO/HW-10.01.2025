8c1ff9845ad1d2d836a4c53d0c59ef22ae55ba3a
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-10 14:10:03 +0500

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program {
+    static void Main() {
+        Console.Write("Enter the number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        long[] fibonachi = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                fibonachi[i] = 0;
+            } else if (i == 1) {
+                fibonachi[i] = 1;
+            } else {
+                fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            Console.Write(fibonachi[i]);
+            if (i < n - 1) {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
